71413c8820dc45b0000dacb8c377d287865a1ab9
shubham91 <shubhambhatia@91social.com>
2021-06-01 16:02:04 +0530

    jdfsj

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div data-testid="todo-1" className="App">
       <button onClick={() => setButton(!button)}> Click me</button>
       {button ===  true ? <h1>Shubham</h1>: null}
+      <p>Shubham</p>
     </div>
   );
 }
